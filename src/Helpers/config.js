98c0d9b54ae1b44aa37ef3fe58185cae6c5147e3
Amirreza Riahi <amirreza.riahi3311@gmail.com>
2025-09-28 08:14:48 +0330

    Update config.js

--- src/Helpers/config.js
+++ src/Helpers/config.js
@@ -1,2 +1,2 @@
-export const API_URL = `https://rickandmortyapi.com/api/character/`
+export const API_URL = `https://rickandmortyapi.com/api/character/`;
 export const API_URL_EPISODE = `https://rickandmortyapi.com/api/episode/`;
